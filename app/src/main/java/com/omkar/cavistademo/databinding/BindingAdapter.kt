13b0cdb186c041a1c099bc4a1cb3d242ffe82370
Omkar Veer <onkareshwar.veer@extentia.com>
2020-08-15 00:47:16 +0530

    Data binding issue resolved Missing permission added for internet verified application is getting the list of images from api

---
+++ app/src/main/java/com/omkar/cavistademo/databinding/BindingAdapter.kt
@@ -0,0 +1,25 @@
+package com.omkar.cavistademo.databinding
+
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+
+
+@BindingAdapter("app:articleImage")
+
+fun bindArticleImage(
+    imageView: AppCompatImageView,
+    url: String?
+) {
+    if (url != null && !url.endsWith("/")) {
+        Glide.with(imageView.context).load(url)
+            .apply(
+                RequestOptions()
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+            ).into(imageView)
+    } else {
+        Glide.with(imageView.context).clear(imageView)
+    }
+}
