c26bc3a5ffbc06aed9965724b8acdcb3d416cac9
Omkar Veer <onkareshwar.veer@extentia.com>
2020-08-15 00:14:49 +0530

    Infinite scroll listener added for recycler view

---
+++ app/src/main/java/com/omkar/cavistademo/ui/custom/EndlessRecyclerOnScrollListener.kt
@@ -0,0 +1,39 @@
+package com.omkar.cavistademo.ui.custom
+
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class EndlessRecyclerOnScrollListener(
+    private var mLinearLayoutManager: GridLayoutManager
+) : RecyclerView.OnScrollListener() {
+    private var previousTotal = 0
+    private var loading = true
+    private var firstVisibleItem = 0
+    private var visibleItemCount = 0
+    private var totalItemCount = 0
+    private var currentPage = 1
+
+    override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+        super.onScrolled(recyclerView, dx, dy)
+        visibleItemCount = recyclerView.childCount
+        totalItemCount = mLinearLayoutManager.itemCount
+        firstVisibleItem = mLinearLayoutManager.findFirstVisibleItemPosition()
+        if (loading) {
+            if (totalItemCount > previousTotal) {
+                loading = false
+                previousTotal = totalItemCount
+            }
+        }
+        val visibleThreshold = 1
+        if (!loading
+            && totalItemCount - visibleItemCount <= firstVisibleItem + visibleThreshold
+        ) {
+            currentPage++
+            onLoadMore(currentPage)
+            loading = true
+        }
+    }
+
+    protected abstract fun onLoadMore(current_page: Int)
+
+}
